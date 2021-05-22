f392a5d94837aeec7b5fb54bd451441a0ccca21d
sandy <sandytang2406@gmail.com>
2021-05-22 22:01:52 +0200

    einlesen der kartenId funktioniert nicht

--- src/main/java/at/htl/nfc/Main.java
+++ src/main/java/at/htl/nfc/Main.java
@@ -1,9 +1,19 @@
 package at.htl.nfc;
 
+import apdu4j.core.BlockingBIBO;
+import apdu4j.core.ResponseAPDU;
+import apdu4j.pcsc.CardBIBO;
 import apdu4j.pcsc.TerminalManager;
 import apdu4j.pcsc.terminals.LoggingCardTerminal;
+
+import javax.smartcardio.Card;
+import javax.smartcardio.CardException;
 import javax.smartcardio.TerminalFactory;
 import javax.smartcardio.CardTerminal;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Main {
@@ -11,9 +21,11 @@ public class Main {
     public static void main(String[] args) {
         TerminalManager.fixPlatformPaths();
 
+        CardTerminal myCardReader = null;
+
         TerminalFactory f = TerminalFactory.getDefault();
-        CardTerminal r = f.terminals().getTerminal("ACR122U");
-        r = LoggingCardTerminal.getInstance(r);
+//        CardTerminal r = f.terminals().getTerminal("ACR122U");
+//        r = LoggingCardTerminal.getInstance(r);
         // Now use javax.smartcardio as you normally do
 
         try {
@@ -23,13 +35,35 @@ public class Main {
             }
             myCardReader = cardTerminalList.get(0);
             System.out.println(myCardReader.getName());
-            System.out.println(myCardReader.isCardPresent()?"Card is present":"Card is NOT present");
+            System.out.println(myCardReader.isCardPresent() ? "Card is present" : "Card is NOT present");
             myCardReader = LoggingCardTerminal.getInstance(myCardReader, System.out);
+
+            // Protocol -> https://de.wikipedia.org/wiki/Application_Protocol_Data_Unit
+            // SCTool.java getBIBO(...)
+            // Protokolle können sein: T=0, T=1, *
+            Card card = myCardReader.connect("T=1");
+            System.out.println("Vor: new BlockingBIBO");
+            BlockingBIBO bibo = new BlockingBIBO(CardBIBO.wrap(card));
+
+            System.out.println("Nach new BlockingBIBO");
+            byte[][] apdus = {};
+            List<byte[]> toCard = new ArrayList<>(Arrays.asList(apdus));
+            System.out.println("Vor: for (byte[] s : toCard)");
+            for (byte[] s : toCard) {
+                System.out.println("In: for (byte[] s : toCard)");
+                ResponseAPDU r = new ResponseAPDU(bibo.transceive(s));
+                if (r.getSW() != 0x9000) {
+                    System.out.println("Card returned " + String.format("%04X", r.getSW()) + ", exiting!");
+                }
+                System.out.println(r.getData());
+            }
 
         } catch (CardException e) {
             e.printStackTrace();
         }
 
     }
+
+
 }
 
