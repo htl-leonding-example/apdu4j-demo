b85a4756f341ad635777ac488df74e7712fe207e
sandy <sandytang2406@gmail.com>
2021-05-22 21:16:29 +0200

    check card is present

--- src/main/java/at/htl/nfc/Main.java
+++ src/main/java/at/htl/nfc/Main.java
@@ -15,6 +15,21 @@ public class Main {
         CardTerminal r = f.terminals().getTerminal("ACR122U");
         r = LoggingCardTerminal.getInstance(r);
         // Now use javax.smartcardio as you normally do
+
+        try {
+            List<CardTerminal> cardTerminalList = f.terminals().list();
+            for (CardTerminal cardTerminal : cardTerminalList) {
+                System.out.println(cardTerminal.getName());
+            }
+            myCardReader = cardTerminalList.get(0);
+            System.out.println(myCardReader.getName());
+            System.out.println(myCardReader.isCardPresent()?"Card is present":"Card is NOT present");
+            myCardReader = LoggingCardTerminal.getInstance(myCardReader, System.out);
+
+        } catch (CardException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
