bc47feadbab69f9f2b30321e69d8ede150ba91fe
sandy <sandytang2406@gmail.com>
2021-05-28 12:33:35 +0200

    optimize loop -Tang Sandy

--- src/main/java/at/htl/nfc/Main.java
+++ src/main/java/at/htl/nfc/Main.java
@@ -2,6 +2,7 @@ package at.htl.nfc;
 
 import apdu4j.core.BlockingBIBO;
 import apdu4j.core.CommandAPDU;
+import apdu4j.core.HexUtils;
 import apdu4j.core.ResponseAPDU;
 import apdu4j.pcsc.CardBIBO;
 import apdu4j.pcsc.TerminalManager;
@@ -31,27 +32,52 @@ public class Main {
 
         try {
             List<CardTerminal> cardTerminalList = f.terminals().list();
-            for (CardTerminal cardTerminal : cardTerminalList) {
-                System.out.println(cardTerminal.getName());
-            }
+//            for (CardTerminal cardTerminal : cardTerminalList) { //Ausgabe alles angesteckten terminals
+//                System.out.println(cardTerminal.getName());
+//            }
             myCardReader = cardTerminalList.get(0);
-            System.out.println(myCardReader.getName());
-           // System.out.println(myCardReader.isCardPresent() ? "Card is present" : "Card is NOT present");
+            System.out.println(myCardReader.getName()); //Ausgabe erstes terminal
+            // System.out.println(myCardReader.isCardPresent() ? "Card is present" : "Card is NOT present");
             myCardReader = LoggingCardTerminal.getInstance(myCardReader, System.out);
 
 
-            Timer timer = new Timer();
-            timer.schedule(new CardPresent(), 0, 5000);
+            try {
+                while (true) {
+
+                    if (myCardReader.isCardPresent()) {
+                        System.out.println("is present");
+                        Card card = myCardReader.connect("T=0");
+
+                        //System.out.println("*");
+                        BlockingBIBO bibo = new BlockingBIBO(CardBIBO.wrap(card));
+                        CommandAPDU cmd = new CommandAPDU("FFCA000000");
+                        byte[] id = bibo.transceive(cmd.getBytes());
+                        ResponseAPDU responseAPDU = new ResponseAPDU(id);
+                        if (responseAPDU.getSW() == 0x9000) {
+                            System.out.printf("UID: %s%n", HexUtils.bin2hex(responseAPDU.getData()));
+                        } else {
+                            System.err.printf("UID bot supported by reader ? SW=%04Xd", responseAPDU.getSW());
+                        }
+                        bibo.close();
+                        //System.out.println("BYTE ARRAY: " + id);
+                        card.disconnect(true);
+                        System.out.println("");
+                    } else {
+                        System.out.println("no card");
+                        System.out.println("");
+                    }
+                    Thread.sleep(1000);
+                }
+            } catch (CardException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
 
             // Protocol -> https://de.wikipedia.org/wiki/Application_Protocol_Data_Unit
             // SCTool.java getBIBO(...)
             // Protokolle können sein: T=0, T=1, *
-
-
-
-
-
 
 
 //            card = myCardReader.connect("T=1");
@@ -89,27 +115,5 @@ public class Main {
 
     }
 
-    static class CardPresent extends TimerTask {
-        public void run( ) {
-            try {
-                if (myCardReader.isCardPresent()) {
-                    System.out.println("is present");
-                    Card card = myCardReader.connect("T=0");
-                    System.out.println("*");
-                    BlockingBIBO bibo = new BlockingBIBO(CardBIBO.wrap(card));
-                    CommandAPDU cmd = new CommandAPDU("FFCA000000");
-                    byte[] id = bibo.transceive(cmd.getBytes());
-                    System.out.println("BYTE ARRAY: " + id);
-                    card.disconnect(true);
-                    System.out.println("");
-                }else {
-
-                    System.out.println("no card");
-                    System.out.println("");
-            }
-            } catch (CardException e) {
-                e.printStackTrace();
-            }
-        }
-    }}
+}
 
